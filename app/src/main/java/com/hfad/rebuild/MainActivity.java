358a37aa258c22bfc185215270bdd2bc67effbaa
Anastasia Konovalova <pochtaplay2013@gmail.com>
2021-12-01 00:16:57 +0300

    auth edit

--- app/src/main/java/com/hfad/rebuild/MainActivity.java
+++ app/src/main/java/com/hfad/rebuild/MainActivity.java
@@ -1,10 +1,15 @@
 package com.hfad.rebuild;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,13 +21,14 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
 
     private EditText username;
-    private  EditText password;
+    private EditText password;
     private Button login;
     private TextView LoginLocked;
     private TextView attempts;
     private TextView numberOfAttempts;
     int numberOfAttemptsCounter = 3;
-    private static int SIGN_IN_CODE =1;
+    private static int SIGN_IN_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,27 +39,35 @@ public class MainActivity extends AppCompatActivity {
         login = (Button) findViewById(R.id.button_login);
 
 
-
-
     }
 
-    public void Login(View view){
+    public void Login(View view) {
 
 
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(FirebaseAuth.getInstance().getCurrentUser() == null){
-                    startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(),SIGN_IN_CODE );
-                }
-                else {
+                if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                    startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE);
+                } else {
 
                     Toast.makeText(getApplicationContext(), "Вход выполнен", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                     startActivity(intent);
                 }
             }
-        };login.setOnClickListener(clickListener);
+        };
+        login.setOnClickListener(clickListener);
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        if (activity != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            if (inputMethodManager != null)
+                if (activity.getCurrentFocus() != null)
+                    if (activity.getCurrentFocus().getWindowToken() != null)
+                        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+        }
     }
 }
